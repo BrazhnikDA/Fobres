4218554d577129809c8653c5a3f8353b7700b307
BrazhnikDA <M1ND228@yandex.ru>
2022-05-03 18:09:47 +0300

    Screen profile

---
+++ app/src/main/java/com/brazhnik/fobres/view/profile/ProfilePresenter.kt
@@ -0,0 +1,31 @@
+package com.brazhnik.fobres.view.profile
+
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import com.brazhnik.fobres.repository.models.ModelProfileHelper
+import com.brazhnik.fobres.repository.storage.StorageProfile
+import com.brazhnik.fobres.repository.viewmodel.VModelProfile
+
+@InjectViewState
+class ProfilePresenter () : MvpPresenter<ProfileView>(), ProfileView {
+
+    private var storage: StorageProfile = StorageProfile()
+    private var modelProfileHelper: ModelProfileHelper = ModelProfileHelper(storage)
+
+    override fun getProfileInfo(): VModelProfile {
+        return modelProfileHelper.getProfileInfo()
+    }
+
+    override fun openHistoryDeposit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewHowGuest() {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeProfile() {
+        TODO("Not yet implemented")
+    }
+
+}
