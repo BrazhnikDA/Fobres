5c258d6723d2a8cbcdc98a73dec3cf0aa5d0989d
BrazhnikDA <M1ND228@yandex.ru>
2022-05-03 18:10:09 +0300

    Start screen

---
+++ app/src/main/java/com/brazhnik/fobres/view/main/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.brazhnik.fobres.view.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.brazhnik.fobres.R
+import com.brazhnik.fobres.view.profile.ProfileActivity
+import com.brazhnik.fobres.view.rating.RatingActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnProfile: Button = findViewById(R.id.button)
+        val btnRating: Button = findViewById(R.id.button2)
+
+        btnProfile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+        btnRating.setOnClickListener {
+            startActivity(Intent(this, RatingActivity::class.java))
+        }
+    }
+}
