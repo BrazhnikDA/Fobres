92e8494ecf60f80c628aa5a1e3302f28a06ff49a
BrazhnikDA <M1ND228@yandex.ru>
2022-05-07 19:41:50 +0300

    File helper

--- app/src/main/java/com/brazhnik/fobres/utilities/Extensions.kt
+++ app/src/main/java/com/brazhnik/fobres/utilities/Extensions.kt
@@ -1,4 +1,19 @@
 package com.brazhnik.fobres.utilities
 
-class Extensions {
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+
+/**
+ * Display message
+ */
+fun Context.displayToast(message: String?) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * Changing visibility
+ */
+fun View.show() {
+    this.visibility = View.VISIBLE
 }
