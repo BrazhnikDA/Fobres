7574f72438f78a435c07aee2bd034bb9c2a19897
BrazhnikDA <M1ND228@yandex.ru>
2022-05-08 13:53:53 +0300

    Update profile

--- app/src/main/java/com/brazhnik/fobres/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/brazhnik/fobres/view/profile/ProfileActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.brazhnik.fobres.R
 import com.brazhnik.fobres.repository.data.Profile
@@ -16,6 +18,8 @@ class ProfileActivity : AppCompatActivity(), ProfileView {
 
     private var mainAdapter: ProfileAdapter? = null
 
+    private var profile: MutableLiveData<Profile> = MutableLiveData()
+    private var id: Int = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,13 +27,14 @@ class ProfileActivity : AppCompatActivity(), ProfileView {
 
         presenter = ProfilePresenter()
 
-        fillingFields()
+        profile = getCurrentProfileAPI(id)
+        profile.observe(this, Observer {
+            profile.value?.let { it1 -> fillingFields(it1) }
+        })
     }
 
     @SuppressLint("SetTextI18n")
-    private fun fillingFields() {
-        val profileInfo = getCurrentProfile()
-
+    private fun fillingFields(profileInfo: Profile) {
         val profileName = findViewById<TextView>(R.id.profileName)
         val profileNickName = findViewById<TextView>(R.id.profileNickName)
         val profileDonate = findViewById<TextView>(R.id.profileDonate)
@@ -42,59 +47,47 @@ class ProfileActivity : AppCompatActivity(), ProfileView {
         val profileCityBody = findViewById<TextView>(R.id.profileCityBody)
         val profileDescriptionBody = findViewById<TextView>(R.id.profileDescriptionBody)
 
-        profileName.text = "${profileInfo.firstName} ${profileInfo.secondName}"
-        profileNickName.text = profileInfo.nickName
-        profileDonate.text = profileInfo.amountAll
-        profileRating.text = profileInfo.placeInRating
-        profileDescription.text = profileInfo.description
+        profileName.text = "${profileInfo.firstName} ${profileInfo.lastName}"
+        profileNickName.text = profileInfo.login
+        profileDonate.text = profileInfo.money.toString()
+        //profileRating.text = profileInfo.placeInRating // Подумать как получать место в рейтинге
+        profileDescription.text = profileInfo.profileDescription
 
-        profileNameBody.text = "${profileInfo.firstName} ${profileInfo.secondName}"
+        profileNameBody.text = "${profileInfo.firstName} ${profileInfo.lastName}"
         profileCountryBody.text = profileInfo.country
         profileCityBody.text = profileInfo.city
-        profileDescriptionBody.text = profileInfo.description
-        /*val profileInfo = getProfileInfo()
-
-        val profileName = findViewById<TextView>(R.id.profileName)
-        val profileNickName = findViewById<TextView>(R.id.profileNickName)
-        val profileDonate = findViewById<TextView>(R.id.profileDonate)
-        val profileRating = findViewById<TextView>(R.id.profileRating)
-        val profileDescription = findViewById<TextView>(R.id.profileDescription)
-
-
-        val profileNameBody = findViewById<TextView>(R.id.profileNameBody)
-        val profileCountryBody = findViewById<TextView>(R.id.profileCountryBody)
-        val profileCityBody = findViewById<TextView>(R.id.profileCityBody)
-        val profileDescriptionBody = findViewById<TextView>(R.id.profileDescriptionBody)
-
-        profileName.text = "${profileInfo.firstName} ${profileInfo.secondName}"
-        profileNickName.text = profileInfo.nickName
-        profileDonate.text = profileInfo.amountAll
-        profileRating.text = profileInfo.placeInRating
-        profileDescription.text = profileInfo.description
-
-        profileNameBody.text = "${profileInfo.firstName} ${profileInfo.secondName}"
-        profileCountryBody.text = profileInfo.country
-        profileCityBody.text = profileInfo.city
-        profileDescriptionBody.text = profileInfo.description*/
+        profileDescriptionBody.text = profileInfo.profileDescription
     }
 
-    override fun getProfileInfo(): Profile {
-        return presenter.getProfileInfo()
+    override fun getCurrentProfileAPI(id: Int): MutableLiveData<Profile> {
+        return presenter.getCurrentProfileAPI(id)
     }
 
-    override fun getCurrentProfile(): Profile {
-        return presenter.getCurrentProfile()
+    override fun getHistoryDepositAPI(id: Int) {
+        return presenter.getHistoryDepositAPI(id)
     }
 
-    override fun openHistoryDeposit() {
-        TODO("Not yet implemented")
+    override fun getViewHowGuestAPI(id: Int) {
+        return presenter.getViewHowGuestAPI(id)
     }
 
-    override fun viewHowGuest() {
-        TODO("Not yet implemented")
+    override fun updateProfileAPI(id: Int) {
+        return presenter.updateProfileAPI(id)
     }
 
-    override fun changeProfile() {
-        TODO("Not yet implemented")
+    override fun getCurrentProfileDB(id: Int): Profile {
+        return presenter.getCurrentProfileDB(id)
+    }
+
+    override fun getHistoryDepositDB(id: Int) {
+        return presenter.getHistoryDepositDB(id)
+    }
+
+    override fun getViewHowGuestDB(id: Int) {
+        return presenter.getViewHowGuestDB(id)
+    }
+
+    override fun updateProfileDB(id: Int) {
+        return presenter.updateProfileDB(id)
     }
 }
