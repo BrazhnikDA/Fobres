86050d66af6efb2c100ad55c05bdf7db603b0ba7
BrazhnikDA <M1ND228@yandex.ru>
2022-06-26 10:38:01 +0300

    New query for logout or take country/city current profile

--- app/src/main/java/com/brazhnik/fobres/data/database/room/dao/ProfileEventDao.kt
+++ app/src/main/java/com/brazhnik/fobres/data/database/room/dao/ProfileEventDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.brazhnik.fobres.data.database.repository.ProfileEventRepository
 import com.brazhnik.fobres.data.database.room.entity.ProfileEventEntity
 
 @Dao
@@ -14,4 +15,13 @@ interface ProfileEventDao {
 
     @Query("SELECT * FROM profile_entity")
     fun getProfile(): List<ProfileEventEntity>
+
+    @Query("DELETE FROM profile_entity WHERE id = :id")
+    fun logout(id: String)
+
+    @Query("SELECT country FROM profile_entity")
+    fun getCountry(): String
+
+    @Query("SELECT city FROM profile_entity")
+    fun getCity(): String
 }
