d87060fb5a1902c2f9ee5aa977d50f8d1a72e7d3
BrazhnikDA <M1ND228@yandex.ru>
2022-07-06 15:07:29 +0300

    Impl get current context for acces to res app

--- app/src/main/java/com/brazhnik/fobres/utilities/App.kt
+++ app/src/main/java/com/brazhnik/fobres/utilities/App.kt
@@ -1,4 +1,19 @@
 package com.brazhnik.fobres.utilities
 
-class App {
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        mContext = applicationContext
+    }
+
+    companion object {
+        private var mContext: Context? = null
+        val resources: Resources?
+            get() = mContext?.resources
+    }
 }
