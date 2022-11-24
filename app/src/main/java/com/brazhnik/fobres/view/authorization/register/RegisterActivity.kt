7ba15848d01fc2e6fbb18e7449bbe3ea4a4e9714
DESKTOP-KEAG5H6\Dima <dimitry.brazhnik@yandex.ru>
2022-11-24 14:27:51 +0300

    Added vvalidator to registration screen and fix validator

--- app/src/main/java/com/brazhnik/fobres/view/authorization/register/RegisterActivity.kt
+++ app/src/main/java/com/brazhnik/fobres/view/authorization/register/RegisterActivity.kt
@@ -2,6 +2,7 @@ package com.brazhnik.fobres.view.authorization.register
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -14,11 +15,16 @@ import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.brazhnik.fobres.data.SharedData
 import com.brazhnik.fobres.data.model.RegistrationUser
 import com.brazhnik.fobres.databinding.ActivityRegisterBinding
+import com.brazhnik.fobres.utilities.Validator
 import com.brazhnik.fobres.utilities.displayToast
 import com.brazhnik.fobres.view.authorization.login.LoginPresenter
 import com.brazhnik.fobres.view.main.MainActivity
 
 class RegisterActivity : AppCompatActivity(), RegisterView {
+
+    private val validator = Validator()
+    private var listError: HashMap<Int, String> = hashMapOf()
+
     private lateinit var binding: ActivityRegisterBinding
 
     @InjectPresenter
@@ -39,20 +45,38 @@ class RegisterActivity : AppCompatActivity(), RegisterView {
         val buttonRegister = binding.buttonRegister
 
         buttonRegister.setOnClickListener {
-            val user = RegistrationUser(
-                login = binding.etLogin.text.toString(),
-                firstName = binding.etFirstName.text.toString(),
-                lastName = binding.etLastName.text.toString(),
-                country = binding.etCountry.text.toString(),
-                city = binding.etCity.text.toString(),
-                password = binding.etPassword.text.toString()
-            )
+            val login = binding.etLogin.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmPassword = binding.etPasswordConfirm.text.toString()
+            val firstName = binding.etFirstName.text.toString()
+            val lastName = binding.etLastName.text.toString()
 
-            registerPresenter.registrationUser(user)
+            listError.clear()
+            listError.putAll(validator.isValidLogin(login))
+            listError.putAll(validator.isValidPassword(password))
+            listError.putAll(validator.isValidConfirmPassword(password, confirmPassword))
+            listError.putAll(validator.isValidName(firstName))
+            listError.putAll(validator.isValidName(lastName))
+            if(listError.size == 0) {
+                binding.textViewListError.visibility = View.GONE
+                val user = RegistrationUser(
+                    login = binding.etLogin.text.toString(),
+                    firstName = binding.etFirstName.text.toString(),
+                    lastName = binding.etLastName.text.toString(),
+                    country = binding.etCountry.text.toString(),
+                    city = binding.etCity.text.toString(),
+                    password = binding.etPassword.text.toString()
+                )
+
+                registerPresenter.registrationUser(user)
+            } else {
+                binding.textViewListError.visibility = View.VISIBLE
+                binding.textViewListError.text = validator.textForShowScreen(listError)
+            }
         }
 
         registerPresenter.response.observe(this) {
-            this.displayToast("УСПЕХ" + it.token)
+            this.displayToast("Быстрее проверь свой рейтинг!")
             SharedData._userToken = "Bearer " + it.token
             registerPresenter.findUserByLogin(it.username)
         }
